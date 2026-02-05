d527b6509ed8eb9eb0e557742929f60e1b451cb4
Xach Zakaryan <152504153+XachZak777@users.noreply.github.com>
2026-02-05 12:40:06 +0400

    00: Initial Setup (#1)
    * Initial setup
    
    * Adding Carrier Service
    
    ---------
    
    Co-authored-by: kzakaryan <kzakaryan@griddynamics.com>

---
+++ src/main/java/am/loadboardbackend/dto/RegisterResponse.java
@@ -0,0 +1,12 @@
+package am.loadboardbackend.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegisterResponse {
+
+    private String message;
+}
